fc99e940525249ef3208b020b6736aadbe37d377
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-06-15 22:40:00 +0100

    FAI-3447 - Slight restructure and pull in Azure table storage config

--- src/SFA.DAS.FundingRuleBridge/Program.cs
+++ src/SFA.DAS.FundingRuleBridge/Program.cs
@@ -1,22 +1,9 @@
-using Azure.Messaging.ServiceBus;
-using Azure.Monitor.OpenTelemetry.Exporter;
-using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
-using Microsoft.Azure.Functions.Worker.OpenTelemetry;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OpenTelemetry;
+using SFA.DAS.FundingRuleBridge.Jobs.Core;
 
-var builder = FunctionsApplication.CreateBuilder(args);
-
-builder.ConfigureFunctionsWebApplication();
-
-builder.Services.AddOpenTelemetry()
-    .UseFunctionsWorkerDefaults()
-    .UseAzureMonitorExporter();
-
-builder.Services.AddSingleton(sp =>
-    new ServiceBusClient(sp.GetRequiredService<IConfiguration>()["ServiceBusConnection"]));
-
-builder.Build().Run();
+FunctionsApplication
+    .CreateBuilder(args)
+    .ConfigureFundingRuleBridgeApp()
+    .Build()
+    .Run();
