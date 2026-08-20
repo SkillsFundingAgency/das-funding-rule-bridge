3acfffa3849c48895c4cad1f211c6be8d43b2aa7
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2026-08-20 12:14:14 +0100

    FAI-3573 - Add queue creation into code.

---
+++ src/SFA.DAS.FundingRuleBridge/Infrastructure/ServiceBusQueueInitialiser.cs
@@ -0,0 +1,41 @@
+using Azure.Messaging.ServiceBus.Administration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SFA.DAS.FundingRuleBridge.Jobs.Core;
+
+namespace SFA.DAS.FundingRuleBridge.Jobs.Infrastructure;
+
+public class ServiceBusQueueInitialiser(ServiceBusAdministrationClient adminClient, ILogger<ServiceBusQueueInitialiser> logger) : IHostedService
+{
+    private static readonly string[] _queuesToCreate =
+    [
+        QueueConstants.ValidationRequestsQueue,
+        QueueConstants.ValidationCallbackQueue
+    ];
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        var options = new CreateQueueOptions(string.Empty)
+        {
+            LockDuration = TimeSpan.FromMinutes(5),
+            DefaultMessageTimeToLive = TimeSpan.FromDays(14),
+            DeadLetteringOnMessageExpiration = true,
+            MaxDeliveryCount = 5,
+            DuplicateDetectionHistoryTimeWindow = TimeSpan.FromMinutes(10),
+            RequiresDuplicateDetection = false,
+            RequiresSession = false
+        };
+
+        foreach (var queueName in _queuesToCreate)
+        {
+            if (await adminClient.QueueExistsAsync(queueName, cancellationToken))
+                continue;
+
+            options.Name = queueName;
+            await adminClient.CreateQueueAsync(options, cancellationToken);
+            logger.LogInformation("Created Service Bus queue '{QueueName}'", queueName);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
