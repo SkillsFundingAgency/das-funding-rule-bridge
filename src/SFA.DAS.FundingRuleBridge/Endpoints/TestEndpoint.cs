ea2c6832fd2ef6151c44cd7d6041b979ca6a048f
killij <51908793+killij@users.noreply.github.com>
2026-09-03 10:16:30 +0100

    Add test endpoint

---
+++ src/SFA.DAS.FundingRuleBridge/Endpoints/TestEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.DurableTask;
+using Microsoft.DurableTask.Client;
+using Microsoft.Extensions.Logging;
+
+namespace SFA.DAS.FundingRuleBridge.Jobs.Endpoints;
+
+public class TestEndpoint(ILogger<TestEndpoint> logger)
+{
+    [Function(nameof(TestEndpoint))]
+    public async Task Test([TimerTrigger("0 0 29 2 1")] TimerInfo timer, [DurableClient] DurableTaskClient durableClient)
+    {
+        var parameters = new Dictionary<string, string>
+        {
+            { "TestId", Guid.NewGuid().ToString() }
+        };
+
+        using var _ = logger.BeginScope(parameters);
+        logger.LogInformation("Scheduling orchestrator");
+
+        var instanceId = await durableClient.ScheduleNewOrchestrationInstanceAsync(nameof(TestOrchestrator));
+        
+        using (logger.BeginScope(new Dictionary<string, string> { { "InstanceId", instanceId }, { "TestId", Guid.NewGuid().ToString() } }))
+        {
+            logger.LogInformation("Orchestration scheduled");
+        }
+    }
+}
+public class TestOrchestrator
+{
+    [Function(nameof(TestOrchestrator))]
+    public static async Task<bool> RunOrchestrator([OrchestrationTrigger] TaskOrchestrationContext context)
+    {
+        var logger = context.CreateReplaySafeLogger<TestOrchestrator>();
+        var parameters = new Dictionary<string, string>
+        {
+            { "TestId", Guid.NewGuid().ToString() }
+        };
+        
+        using var _ = logger.BeginScope(parameters);
+        logger.LogInformation("Executing orchestrator");
+        
+        using (logger.BeginScope(new Dictionary<string, string> { { "TestId", Guid.NewGuid().ToString() } }))
+        {
+            logger.LogInformation("Orchestration execution completed");
+        }
+        return true;
+    }
+}
