95aa148c9bbf17f40813a47891dac9fb26e59701
killij <51908793+killij@users.noreply.github.com>
2026-09-04 10:52:36 +0100

    More changes

--- src/SFA.DAS.FundingRuleBridge/Core/OpenTelemetryExtensions.cs
+++ src/SFA.DAS.FundingRuleBridge/Core/OpenTelemetryExtensions.cs
@@ -1,13 +1,9 @@
 using System.Diagnostics.CodeAnalysis;
-using Azure.Monitor.OpenTelemetry.AspNetCore;
 using Azure.Monitor.OpenTelemetry.Exporter;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Azure.Functions.Worker.OpenTelemetry;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using OpenTelemetry.Logs;
 
 namespace SFA.DAS.FundingRuleBridge.Jobs.Core;
 
@@ -21,11 +17,11 @@ public static class OpenTelemetryExtensions
             return;
         }
 
-        builder.Logging.AddOpenTelemetry(opt => opt.IncludeScopes = true);
-        builder
-            .Services
+        builder.Services
             .AddOpenTelemetry()
             .UseFunctionsWorkerDefaults()
             .UseAzureMonitorExporter(opt => opt.ConnectionString = connectionString);
+        builder.Logging
+            .AddOpenTelemetry(opt => opt.IncludeScopes = true);
     }
 }
